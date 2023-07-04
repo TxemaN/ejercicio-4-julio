d883f2fdcd5de93178d6541735f46af6121319f9
TxemaN <txalcala@hotmail.com>
2023-07-04 21:03:35 +0200

    objeto pelicula se acumula en array

--- js/main.js
+++ js/main.js
@@ -1,3 +1,34 @@
+const formulario = document.querySelector("#pelisForm")
+const titulillo = document.querySelector("#titulo")
+const directorcillo = document.querySelector("#director")
+const generozuelo = document.querySelector("#genero")
+const boton = document.querySelector('#boton')
+
+const arrayPelisUsuario=[{}]
+
+let elemento = {
+  titulo: "",
+  director: "",
+  anio: "",
+  genero: "",
+}
+formulario.addEventListener('submit',(ev)=>{
+		ev.preventDefault()
+		validar();
+	});
 
 
-arrayPelisUsuario= [{}];
+  const validar=()=>{
+
+		elemento.titulo=titulillo.value;
+		elemento.director=directorcillo.value;
+    elemento.anio=document.getElementById("anio").value;
+    elemento.genero=generozuelo.value;
+    return arrayPelisUsuario.push(arrayPelisUsuario)
+
+		
+		}
+
+
+
+console.log(validar())
