bb65b83a0c8def185bfa97ef393e2ab95766df9d
TxemaN <txalcala@hotmail.com>
2023-07-04 23:50:22 +0200

    dibujando la tabla

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const form = document.querySelector("#pelisForm")
 const boton = document.querySelector('#boton')
+const contenedorPelis = document.querySelector("#contenedor-pelis");
+fragment = document.createDocumentFragment
 
-const arrayPelisUsuario=[{}]
+const arrayPelisUsuario = [{}]
 
 let elemento = {
   titulo: "",
@@ -9,23 +11,37 @@ let elemento = {
   anio: "",
   genero: "",
 }
-form.addEventListener('submit',(ev)=>{
-		ev.preventDefault()
-		validar();
-	});
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault()
+  empujar();
+});
+
+//Aquí empujamos los datos de la pel al array//
+const empujar = () => {
+
+  elemento.titulo = document.getElementById("titulo").value;
+  elemento.director = document.getElementById("director").value;
+  elemento.anio = document.getElementById("anio").value;
+  elemento.genero = document.getElementById("genero").value;
+  arrayPelisUsuario.push(elemento)
 
 
-  const validar=()=>{
+}
 
-		elemento.titulo=document.getElementById("titulo").value;
-		elemento.director=document.getElementById("director").value;
-    elemento.anio=document.getElementById("anio").value;
-    elemento.genero=document.getElementById("genero").value;
-    arrayPelisUsuario.push(arrayPelisUsuario)
+//Aquí usamos el array de pelis para las tablas con las pelis, el género, año...//
+const pintarPelis = () => {
+  arrayPelisUsuario.forEach((item) => {
 
-		
-		}
+    document.getElementById("tituloTabla").textContent=item.titulo;
+    document.getElementById("directorTabla").textContent=item.director;
+    document.getElementById("anioTabla").textContent=item.anio;
+    document.getElementById("generoTabla").textContent=item.genero;
 
+    peli.append(datoPeli);
 
+    fragment.append(peli);
+  })
+  contenedorPelis.append(fragment);
+}
 
-console.log(arrayPelisUsuario,elemento)
+console.log(arrayPelisUsuario, elemento)
